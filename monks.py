85d5f6d43e79ecdeeb3845ea0b53d52ee1fac9d6
Riccardo Massidda <contact@rmassidda.it>
2019-12-29 13:11:00 +0100

    Validation module

--- monks.py
+++ monks.py
@@ -1,22 +1,6 @@
-from concurrent.futures import ThreadPoolExecutor
-from grid import Grid
-from network import Network
-from utils import onehot
+from validation import Validation
 import numpy as np
 import sys
-
-def validate(par):
-    nn = Network(**par,epochs=500)
-
-    # Split with validation set (75/25)
-    bound = int(len(train_x)*(3/4))
-    nn.train(train_x[:bound],train_y[:bound])
-
-    valid_x = train_x[bound:]
-    valid_y = train_y[bound:]
-
-    estimated_risk = nn.compute_misclassified(valid_x,valid_y)
-    return par,estimated_risk
 
 train_fp = sys.argv[1]
 test_fp = sys.argv[2]
@@ -26,6 +10,9 @@ monk_ranges = [3,3,2,3,4,2]
 raw_x = np.genfromtxt(train_fp,usecols=(1,2,3,4,5,6))
 train_x = np.array(list(map(lambda x: onehot(x,monk_ranges),raw_x)))
 train_y = np.genfromtxt(train_fp,usecols=(0))
+raw_x = np.genfromtxt(test_fp,usecols=(1,2,3,4,5,6))
+test_x = np.array(list(map(lambda x: onehot(x,monk_ranges),raw_x)))
+test_y = np.genfromtxt(test_fp,usecols=(0))
 
 # Hyperparameters
 hp = [{
@@ -34,30 +21,7 @@ hp = [{
     'topology': [[17,1,1],[17,2,1],[17,4,1],[17,8,1]]
     }]
 
-# Model selection
-grid = Grid(hp)
-risk = np.Inf
-best = None
-
-# Parallel grid search
-print("=== MODEL SELECTION ===")
-with ThreadPoolExecutor(max_workers=8) as executor:
-    grid_res = executor.map(validate,grid)
-    for p in grid_res:
-        print(p,sep='\t')
-        if p[1] < risk:
-            risk = p[1]
-            best = p[0]
-
-# Train on the best model
-nn = Network(**best,epochs=500)
-nn.train(train_x,train_y)
-
-raw_x = np.genfromtxt(test_fp,usecols=(1,2,3,4,5,6))
-test_x = np.array(list(map(lambda x: onehot(x,monk_ranges),raw_x)))
-test_y = np.genfromtxt(test_fp,usecols=(0))
-
-# Model assessment
-print("=== MODEL ASSESSMENT ===")
-estimated_risk = nn.compute_misclassified(test_x,test_y)
-print(best, estimated_risk,sep='\t')
+# Validation
+val = Validation(train_x,train_y)
+model = val.model_selection(hp)
+risk = val.model_assessment(model,test_x,test_y)
---
+++ validation.py
@@ -0,0 +1,55 @@
+from concurrent.futures import ThreadPoolExecutor
+from grid import Grid
+from network import Network
+from utils import onehot
+import numpy as np
+import sys
+
+class Validation:
+    def __init__(self,train_x,train_y,verbose=True):
+        self.train_x = train_x
+        self.train_y = train_y
+        self.verbose = verbose
+
+    def hold_out(self,par):
+        nn = Network(**par)
+
+        # Split with validation set (75/25)
+        bound = int(len(self.train_x)*(3/4))
+
+        nn.train(self.train_x[:bound],self.train_y[:bound])
+
+        valid_x = self.train_x[bound:]
+        valid_y = self.train_y[bound:]
+
+        estimated_risk = nn.compute_misclassified(valid_x,valid_y)
+        return par,estimated_risk
+
+    def model_selection(self,hp):
+        grid = Grid(hp)
+        risk = np.Inf
+        best = None
+
+        # Parallel grid search
+        if self.verbose:
+            print("=== MODEL SELECTION ===")
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            grid_res = executor.map(self.hold_out,grid)
+            for p in grid_res:
+                if self.verbose:
+                    print(p,sep='\t')
+                if p[1] < risk:
+                    risk = p[1]
+                    best = p[0]
+        return best
+
+    def model_assessment(self,model,test_x,test_y):
+        # Train on the best model
+        if self.verbose:
+            print("=== MODEL ASSESSMENT ===")
+        nn = Network(**model)
+        nn.train(self.train_x,self.train_y)
+        risk = nn.compute_misclassified(test_x,test_y)
+        if self.verbose:
+            print(model, risk,sep='\t')
+        return risk
