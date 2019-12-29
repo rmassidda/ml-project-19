15e6f02c8e74bb5fa039d812555f8d62010f4e1a
Riccardo Massidda <contact@rmassidda.it>
2019-12-29 15:47:05 +0100

    Cross validation

--- validation.py
+++ validation.py
@@ -6,10 +6,11 @@ import numpy as np
 import sys
 
 class Validation:
-    def __init__(self,train_x,train_y,loss,verbose=True):
+    def __init__(self,train_x,train_y,loss,k=5,verbose=True):
         self.train_x = train_x
         self.train_y = train_y
         self.loss = loss
+        self.k = k
         self.verbose = verbose
 
     def hold_out(self,par):
@@ -26,6 +27,22 @@ class Validation:
         estimated_risk = nn.error(valid_x,valid_y,self.loss)
         return par,estimated_risk
 
+    def cross_validation(self,par):
+        nn = Network(**par)
+        batch = np.int64(np.floor(len(self.train_x)/self.k))
+        risk = 0
+        for i in range(self.k):
+            # Range of the current validation part
+            rows = range(i*batch,(i+1)*batch)
+            tr_x = np.delete(self.train_x,rows,0)
+            tr_y = np.delete(self.train_y,rows,0)
+            vl_x = self.train_x[rows]
+            vl_y = self.train_y[rows]
+            nn.train(tr_x,tr_y)
+            risk += nn.error(vl_x,vl_y,self.loss)
+        risk /= self.k
+        return par,risk
+
     def model_selection(self,hp):
         grid = Grid(hp)
         risk = np.Inf
@@ -35,7 +52,7 @@ class Validation:
         if self.verbose:
             print("=== MODEL SELECTION ===")
         with ThreadPoolExecutor(max_workers=8) as executor:
-            grid_res = executor.map(self.hold_out,grid)
+            grid_res = executor.map(self.cross_validation,grid)
             for p in grid_res:
                 if self.verbose:
                     print(p,sep='\t')
