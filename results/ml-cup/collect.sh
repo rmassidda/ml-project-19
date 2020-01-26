648385af8539f6c1f53127a79604d66ccbe508fd
Riccardo Massidda <contact@rmassidda.it>
2020-01-26 13:55:35 +0100

    Update comparison script

--- results/ml-cup/collect.sh
+++ results/ml-cup/collect.sh
@@ -1,5 +1,6 @@
 #! sh
 
-echo '----------------------------------' >> comment.txt
-date >> comment.txt
-for fold in exp*; do grep Chosen -A1 $fold/ml-cup.txt | tail -n1 | sed 's/^/'$fold'\t/'; done | sort --field-separator='}' -k2 >> comment.txt
+fn=comparison.txt
+echo '----------------------------------' >> $fn
+date >> $fn
+for fold in exp*; do grep Chosen -A1 $fold/ml-cup.txt | tail -n1 | sed 's/^/'$fold'\t/'; done | sort --field-separator='}' -k2 >> $fn
